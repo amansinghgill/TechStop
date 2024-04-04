fe1d3e1ea86bee73ff56fc51141ab346fc853cdd
Aman Gill <gillaman1998@gmail.com>
2024-04-04 13:22:00 -0400

    Implement authentication logic in authUser controller

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,22 @@ connectDB();
 
 const app = express();
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-
+// Define a simple route for the root URL
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+// Start the server and listen on the specified port
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// Define routes for products and users
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
+//Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to handle 404 Not Found errors
 app.use(notFound);
 app.use(errorHandler);
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
