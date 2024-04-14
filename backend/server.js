426cc15f6d30e239eafc6138fbfe8d9efe61ae7f
Aman Gill <gillaman1998@gmail.com>
2024-04-14 10:56:00 -0400

    Setup Order Routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000; // Setting the port from environment variables or default to 5000
@@ -18,9 +19,10 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 app.use(cookieParser()); // Middleware to parse cookies
 
-// Routes for handling requests to /api/products and /api/users
+// Routes for handling requests to /api/products, /api/users, and /api/orders
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running..."); // Root route that sends a simple response
