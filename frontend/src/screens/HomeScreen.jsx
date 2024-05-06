7ec73e515d1d074e2dc2e82832b76ffcf6e2e6fa
Aman Gill <gillaman1998@gmail.com>
2024-05-06 10:26:00 -0400

    Create an implement Pagination component in HomeScreen and ProductListScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,6 +4,7 @@ import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 
 const HomeScreen = () => {
   const { pageNumber } = useParams();
@@ -32,6 +33,7 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
+          <Paginate pages={data.pages} page={data.page} />
         </>
       )}
     </>
